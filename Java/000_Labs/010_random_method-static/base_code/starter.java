9c01a23e3a059141fb3fb70d8ee58bb6cde91cc6
fishingapple7 <Bradleyjlai@gmail.com>
2026-09-14 21:55:14 +0000

    update

--- Java/000_Labs/010_random_method-static/base_code/starter.java
+++ Java/000_Labs/010_random_method-static/base_code/starter.java
@@ -10,8 +10,14 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		int nine = (int)Math.random() * 9;
-		System.out.print("A number between 0 - 9: " + nine);
+		int nine = (int)(Math.random() * 10);
+		int hundred = (int)(Math.random() * 101 + 1);
+		double two = (Math.random() * 1 + 2.5);
+		double four = (Math.random() * 575 + 14);
+		System.out.println("A number between 0 - 9: " + nine);
+		System.out.println("A number between 1 - 100: " + hundred);
+		System.out.println("A number between 2.5 - 3.5: " + two);
+		System.out.println("A number between 14 - 589: " + four);
 		//System.out.print("A number between 1 - 10: " + ); 
 		//System.out.print("A number between 2.5 and 3.5: " + ); 
 		//System.out.print("A double between 14 and 589: " + );  
